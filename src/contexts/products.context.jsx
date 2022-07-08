22f69067e462a482ad0f9689136a599ddbf71393
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-08 21:04:32 +0200

    Updated firebase.utils with addCollectionAndDocuments and batch and also updated shop-data as well as products.context

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,16 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+//import SHOP_DATA from "../shop-data.js"; //I don't need this anymore as the useEffect is commented out
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  /* useEffect is commented out because everytime I run it it's going to set 
+  new values inside of the database which I don't want to do. I just want to do as a one of thing. */
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []); 
+
   const value = { products };
 
   return (
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
