f3467c4f6200f45f70c7dff8da77ce40572501ac
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-10 16:36:15 +0200

    Added the Styled components to form-input and cart-icon

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,19 +1,18 @@
-import "./form-input.styles.scss";
+import { FormInputLabel, Input, Group } from "./form-input.styles";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
-      {label && ( //if label exist then render this label
-        <label
-          className={`${
-            otherProps.value.length ? "shrink" : ""
-          } form-input-label`}
-        >
+    <Group>
+      <Input {...otherProps} />
+      {label && ( //If label exist then render this label
+        /*If the number of the length is O then this would be falsy, 
+        if it's greater than 0 it would be truthy and shrink would be true 
+        inside of my shrink value prop of my form-input.styles */
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
---
+++ src/components/form-input/form-input.styles.jsx
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+
+const subColor = "grey";
+const mainColor = "black";
+
+const shrinkLabelStyles = css`
+  top: -14px;
+  font-size: 12px;
+  color: ${mainColor};
+`;
+
+export const FormInputLabel = styled.label`
+  //I need to declare the FormInputLabel first/before the Input, because I need it inside of it.
+  color: ${subColor}; //Ref to ligne 4
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
+  ${({ shrink }) =>
+    shrink &&
+    shrinkLabelStyles}//If shrink is true, I want to add in the shrinkLabelStyles
+`;
+
+export const Input = styled.input`
+  background: none;
+  background-color: white;
+  color: ${subColor}; //Ref to ligne 4
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid ${subColor}; //Ref to ligne 4;
+  margin: 25px 0;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus ~ ${FormInputLabel} {
+    ${shrinkLabelStyles};
+  }
+`;
+
+export const Group = styled.div`
+  position: relative;
+  margin: 45px 0;
+
+  input[type="password"] {
+    letter-spacing: 0.3em;
+  }
+`;
