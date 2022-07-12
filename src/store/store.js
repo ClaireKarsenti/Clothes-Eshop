c97801f04ad94434ef29614a9f9347b49f64e093
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-12 23:52:02 +0200

    Added redux-persist to shop.js as well as redux devTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,45 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-/*Logger allows me to see in the console what the state looks like before an action is dispatch, 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger"; /*Logger allows me to see in the console what the state looks like before an action is dispatch, 
 what the action is and then how the state intern look after the action*/
 
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type:", action.type);
-  console.log("payload:", action.payload);
-  console.log("currentState:", store.getState());
-
-  next(action);
-
-  console.log("next state:", store.getState());
+//I want to keep the item in the cart when I refresh. For that I need to use redux-persist.
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"], //I don't want to persist the user so I blacklist it.
 };
 
-const middleWares = [loggerMiddleware]; // middleWares is kind of a library helpers that run before an action hit the reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+/* middleWares is kind of a library helpers that run before an action hit the reducer. 
+I want to run this logger from middleWares only when I am in development and not when I am in production. 
+For that I need to leverage the process.env.NODE_ENV that tell me wether not I am in development or in prodcution
+based on the string 'development' or 'production' */
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+//I use Redux DevTools
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 //I generate the store :
-export const store = createStore(rootReducer, undefined, composedEnhancers);
-/* I need a rootReducer in order to generate the store.
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+/* I need a persistedReducer in order to generate the store.
 The second argument is a default state (optionnal).
 The thirs argument is the middleWares */
+
+export const persistor = persistStore(store);
