6d3403b8d3c0281329981f53a2b3ebdea7f32f17
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-07-21 17:13:06 +0200

    Added payment-form component, updated button styles and component. Updated checkout with PaymentForm. Stripe.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -21,7 +23,9 @@ root.render(
         {/* loading=null means that it will render nothing until persist is actually finished
          */}
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,8 +1,9 @@
 import { all, call } from "redux-saga/effects";
 
 import { categoriesSaga } from "./categories/category.saga";
+import { userSagas } from "./user/user.saga"; //For the connection
 
 //Because of the * it's a generator function
 export function* rootSaga() {
-  yield all([call(categoriesSaga)]);
+  yield all([call(categoriesSaga), call(userSagas)]);
 }
